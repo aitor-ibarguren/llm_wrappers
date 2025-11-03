06003a90c2ff44cc484dacd80717796bed4fde2b
Aitor Ibarguren <aitor.ibarguren.s@gmail.com>
2025-11-03 22:28:51 +0100

    25 add hybrid search (#26)
    * Added TF-IDF for keyword management
    
    * Added keyword and hybrid search
    
    * Updated requirements file
    
    * Fixed langchain text splitter import
    
    * Fixed langchain text splitter import II
    
    * Flake8 fixes

--- faiss_wrapper/faiss_wrapper.py
+++ faiss_wrapper/faiss_wrapper.py
@@ -1,3 +1,4 @@
+import ast
 import os
 import random
 import re
@@ -6,12 +7,16 @@ from urllib.parse import urlparse
 
 import faiss
 import fitz  # For PyMuPDF
+import numpy as np
 import pandas
 import requests
 from bs4 import BeautifulSoup
 from ddgs import DDGS
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from scipy.sparse import csr_matrix, load_npz, save_npz
 from sentence_transformers import SentenceTransformer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 from tqdm import tqdm
 
 
@@ -48,12 +53,14 @@ class FAISSWrapper:
             "privacy policy", "navigation", "menu", "footer", "all rights reserved", "©",
         ]
 
-    def init_new_index(self) -> bool:
+    def init_new_index(self, keyword_number: int = 3) -> bool:
         # Check if already initialized
         if self._index_init:
             print(self._RED + 'Index already initialized/loaded!' + self._RST)
             return False
 
+        # Store keyword number value
+        self._keyword_number = keyword_number
         # Init model
         self._model = SentenceTransformer(self._model_name)
         # Init index
@@ -62,6 +69,9 @@ class FAISSWrapper:
 
         self._index = faiss.IndexHNSWFlat(dimensions, 16)
         self._texts = []
+        self._tf_idf_vectorizer = None
+        self._tfidf_matrix = None
+        self._keywords = []
 
         self._index_init = True
 
@@ -82,19 +92,23 @@ class FAISSWrapper:
 
         return False
 
-    def load_stored_index(self, path: str, index_name: str) -> bool:
+    def load_stored_index(self, path: str, index_name: str,
+                          keyword_number: int = 3) -> bool:
         # Check if already initialized
         if self._index_init:
             print(self._RED + 'Index already initialized/loaded!' + self._RST)
             return False
 
+        # Store keyword number value
+        self._keyword_number = keyword_number
+
         # Ensure the path ends with a '/'
-        if not path.endswith('/'):
-            path += '/'
+        path = path if path.endswith('/') else path + "/"
 
         # Manage paths and file names
         complete_index_path = path + index_name + '.faiss'
         complete_raw_text_path = path + index_name + '.csv'
+        complete_tf_idf_path = path + index_name + '.npz'
 
         # Load index
         print(f"Loading index '{index_name}' from '{path}'...")
@@ -123,17 +137,17 @@ class FAISSWrapper:
 
         self._index_init = True
 
-        # Load raw text CSV
+        # Manage CSV
         try:
+            # Load raw text CSV
             csv_data = pandas.read_csv(complete_raw_text_path, sep=';')
-        except Exception as e:
-            return self._handle_csv_load_error(e, complete_raw_text_path)
-
-        # Get values of provided field
-        try:
+            # Get text
             self._texts = csv_data['text'].tolist()
-        except KeyError:
-            print(self._RED + 'Error parsing raw text CSV file' + self._RST)
+            # Get keywords
+            csv_data['keywords'] = csv_data['keywords'].apply(ast.literal_eval)
+            self._keywords = csv_data['keywords'].tolist()
+        except Exception as e:
+            print(self._RED + 'Error loading/parsing CSV file: ' + e + self._RST)
             return False
 
         # Check row numbers
@@ -141,6 +155,13 @@ class FAISSWrapper:
             print(self._RED +
                   "Index and raw data files' row number do not match" +
                   self._RST)
+            return False
+
+        # Load NPZ file
+        try:
+            self._tfidf_matrix = load_npz(complete_tf_idf_path)
+        except Exception as e:
+            print(self._RED + 'Error loading NPZ file: ' + e + self._RST)
             return False
 
         # Output
@@ -168,6 +189,30 @@ class FAISSWrapper:
 
         return all_chunks
 
+    def _extract_matrix_and_keywords(self, texts: list[str],
+                                     top_n: int = 3
+                                     ) -> tuple[csr_matrix, list[list[str]]]:
+        # Init list
+        keywords = []
+
+        # Check if TF-IDF Vectorizer init
+        if self._tf_idf_vectorizer is None:
+            # Initialize TF-IDF Vectorizer
+            self._tf_idf_vectorizer = TfidfVectorizer(stop_words='english')
+        # Calculate matrix
+        tfidf_matrix = self._tf_idf_vectorizer.fit_transform(texts)
+        # Get words
+        words = self._tf_idf_vectorizer.get_feature_names_out()
+        # Loop in texts
+        for doc_index, text in enumerate(texts):
+            tf_idf_vector = tfidf_matrix[doc_index]
+            sorted_idx = np.argsort(tf_idf_vector.data)[::-1][:top_n]
+            keywords.append(
+                [words[tf_idf_vector.indices[i]] for i in sorted_idx]
+            )
+
+        return tfidf_matrix, keywords
+
     def add_to_index(self, texts: list[str], chunking: bool = False,
                      chunk_size: int = 256, chunk_overlap: int = 25) -> bool:
         # Check if already initialized
@@ -192,6 +237,11 @@ class FAISSWrapper:
 
         # Store original texts
         self._texts.extend(_texts)
+
+        # Extract & store keywords
+        tfidf_matrix, keywords = self._extract_matrix_and_keywords(self._texts)
+        self._tfidf_matrix = tfidf_matrix
+        self._keywords = keywords
 
         # Output
         print(
@@ -252,6 +302,11 @@ class FAISSWrapper:
 
         # Store original texts
         self._texts.extend(texts)
+
+        # Extract & store matrix and keywords
+        tfidf_matrix, keywords = self._extract_matrix_and_keywords(self._texts)
+        self._tfidf_matrix = tfidf_matrix
+        self._keywords = keywords
 
         # Output
         print(
@@ -319,6 +374,11 @@ class FAISSWrapper:
 
         # Store original texts
         self._texts.extend(chunks)
+
+        # Extract & store matrix and keywords
+        tfidf_matrix, keywords = self._extract_matrix_and_keywords(self._texts)
+        self._tfidf_matrix = tfidf_matrix
+        self._keywords = keywords
 
         # Output
         print(
@@ -482,8 +542,6 @@ class FAISSWrapper:
 
         print(f'Chunks after cleaning: {len(chunks)}')
 
-        print(chunks[:10])
-
         # Texts into embedding
         print('Embedding chunks...')
         embeddings = self._model.encode(chunks, convert_to_numpy=True)
@@ -493,6 +551,11 @@ class FAISSWrapper:
 
         # Store original texts
         self._texts.extend(chunks)
+
+        # Extract & store matrix and keywords
+        tfidf_matrix, keywords = self._extract_matrix_and_keywords(self._texts)
+        self._tfidf_matrix = tfidf_matrix
+        self._keywords = keywords
 
         # Output
         print(
@@ -532,6 +595,128 @@ class FAISSWrapper:
 
         return True, output_texts, distances
 
+    def search_by_keyword(
+            self, texts: list[str],
+            top_k: int = 5
+    ) -> tuple[bool, list[list[str]], list[list[float]]]:
+        # Check if already initialized
+        if not self._index_init:
+            print(self._RED + 'Index not initialized/loaded yet!' + self._RST)
+            return False, [], []
+
+        # Check if TF-IDF Vectorizer init
+        if self._tf_idf_vectorizer is None:
+            # Initialize TF-IDF Vectorizer
+            self._tf_idf_vectorizer = TfidfVectorizer(stop_words='english')
+        # Calculate matrix
+        tfidf_matrix = self._tf_idf_vectorizer.transform(texts)
+
+        # Cosine similarity
+        similarity = cosine_similarity(tfidf_matrix, self._tfidf_matrix)
+
+        # Loop in texts
+        output_texts = []
+        output_similarities = []
+        for doc_index, text in enumerate(texts):
+            similarity_vector = similarity[doc_index]
+            sorted_idx = np.argsort(similarity_vector.data)[::-1][:top_k]
+            # Iterate and remove zero similarity values
+            row_texts = []
+            row_similarities = []
+            for i in sorted_idx:
+                if similarity_vector[i] != 0:
+                    row_texts.append(self._texts[i])
+                    row_similarities.append(similarity_vector[i])
+            # Insert in output list
+            output_texts.append(row_texts)
+            output_similarities.append(row_similarities)
+
+        return True, output_texts, output_similarities
+
+    def _normalize_vals(self, distances: list[float]):
+        # Get min and max
+        min_val, max_val = min(distances), max(distances)
+        # Check if min and max are equal
+        if max_val == min_val:
+            return [0.0 for _ in distances]
+
+        return [(val - min_val) / (max_val - min_val) for val in distances]
+
+    def hybrid_search(
+            self, texts: list[str],
+            top_k: int = 5, alpha: int = 0.6
+    ) -> tuple[bool, list[list[str]], list[list[float]]]:
+        # Check if already initialized
+        if not self._index_init:
+            print(self._RED + 'Index not initialized/loaded yet!' + self._RST)
+            return False, [], []
+
+        # Semantic search (top_k x 2 to ensure that a good combined search)
+        res, semantic_texts, semantic_distances = self.search(texts, 2 * top_k)
+
+        # Check result
+        if not res:
+            print(self._RED + 'Error in semantic search!' + self._RST)
+            return False, [], []
+
+        # Keyword search (top_k x 2 to ensure that a good combined search)
+        res, keyword_texts, keyword_similarities = self.search_by_keyword(
+            texts,
+            2 * top_k
+        )
+
+        # Normalize distances
+        normalized_semantic_distances = [self._normalize_vals(
+            sublist) for sublist in semantic_distances]
+        normalized_keyword_similarities = [self._normalize_vals(
+            sublist) for sublist in keyword_similarities]
+
+        # Invert similarity to compare with distances
+        normalized_keyword_similarities_inv = [
+            [1.0 - val for val in list]
+            for list in normalized_keyword_similarities
+        ]
+
+        # Iterate over each search
+        output_texts = []
+        output_distances = []
+        for st, kt, nsd, nksi in zip(semantic_texts, keyword_texts,
+                                     normalized_semantic_distances,
+                                     normalized_keyword_similarities_inv):
+            # Create dictionaries
+            semantic_dict = dict(zip(st, nsd))
+            keyword_dict = dict(zip(kt, nksi))
+
+            # Get all texts
+            all_texts = set(semantic_dict) | set(keyword_dict)
+
+            # Calculate combined distances
+            combined_results = []
+            for text in all_texts:
+                # If text is not found, distance = 1.0
+                semantic_dist = semantic_dict.get(text, 1.0)
+                keyword_dist = keyword_dict.get(text, 1.0)
+                combined_dist = alpha * semantic_dist + \
+                    (1 - alpha) * keyword_dist
+                combined_results.append({"text": text,
+                                         "combined_distance": combined_dist})
+
+            # Sort
+            sorted_combined_distances = sorted(combined_results,
+                                               key=lambda x:
+                                               x["combined_distance"])[:top_k]
+
+            # Extract & insert texts & distances
+            output_texts.append([item["text"]
+                                for item in sorted_combined_distances]
+                                )
+            output_distances.append(
+                [item["combined_distance"]
+                    for item in sorted_combined_distances]
+            )
+
+        return True, output_texts, output_distances
+
     def save_index(self, path: str, index_name: str) -> bool:
         # Check if already initialized
         if not self._index_init:
@@ -545,6 +730,7 @@ class FAISSWrapper:
         # Manage paths and file names
         complete_index_path = path + index_name + '.faiss'
         complete_raw_text_path = path + index_name + '.csv'
+        complete_tf_idf_path = path + index_name + '.npz'
 
         # Save/write index
         try:
@@ -563,8 +749,12 @@ class FAISSWrapper:
 
         # Save raw text data
         df = pandas.DataFrame(self._texts, columns=['text'])
+        df['keywords'] = self._keywords
         df.to_csv(complete_raw_text_path, sep=';',
                   index=True, index_label='ID')
+
+        # Save TF-IDF matrix
+        save_npz(complete_tf_idf_path, self._tfidf_matrix)
 
         # Output
         print('Index and raw text files successfully created!')
