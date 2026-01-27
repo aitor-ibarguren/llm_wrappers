Added precision parameter in all wrappers & fixed tokenizer param in train

gpt2_wrapper/gpt2_wrapper.py
@@ -1,11 +1,13 @@
 import os
 from enum import Enum
+from typing import Literal
 
 import torch
 from datasets import DatasetDict
 from peft import LoraConfig, PeftModel, TaskType, get_peft_model
-from transformers import (DataCollatorForLanguageModeling, GPT2LMHeadModel,
-                          GPT2Tokenizer, Trainer, TrainingArguments)
+from transformers import (BitsAndBytesConfig, DataCollatorForLanguageModeling,
+                          GPT2LMHeadModel, GPT2Tokenizer,
+                          Trainer, TrainingArguments)
 
 
 class GPT2Type(Enum):
@@ -26,6 +28,7 @@ class GPT2Wrapper:
             GPT2Type.XL: "gpt2-xl"
         }
         self._model_name = gpt2_type_names[model_type]
+        self._precision_vals = ["fp32", "fp16", "int8"]
         self._model_init = False
         self._peft_model = False
 
@@ -42,7 +45,15 @@ class GPT2Wrapper:
             print("CUDA not available: Using CPU")
             self._device = torch.device("cpu")
 
-    def load_pretrained_model(self) -> bool:
+    def load_pretrained_model(
+        self,
+        precision: Literal["fp32", "fp16", "int8"] = "fp32"
+    ) -> bool:
+        # Check precision
+        if precision not in self._precision_vals:
+            print(self._RED + "Precision value not valid" + self._RST)
+            return False
+
         # Check if already loaded
         if self._model_init:
             print(self._RED + "Model already loaded!" + self._RST)
@@ -52,7 +63,19 @@ class GPT2Wrapper:
         print(f"Loading model '{self._model_name}'...")
 
         # Load model
-        self._model = GPT2LMHeadModel.from_pretrained(self._model_name)
+        kwargs = {}
+
+        if precision == "int8":
+            bnb_config = BitsAndBytesConfig(load_in_8bit=True)
+            kwargs["quantization_config"] = bnb_config
+        else:
+            dtype = torch.float16 if precision == "fp16" else torch.float32
+            kwargs["dtype"] = dtype
+
+        self._model = GPT2LMHeadModel.from_pretrained(
+            self._model_name,
+            **kwargs
+        )
         # Init tokenizer
         self._tokenizer = GPT2Tokenizer.from_pretrained(self._model_name)
         self._tokenizer.pad_token = self._tokenizer.eos_token
@@ -69,7 +92,14 @@ class GPT2Wrapper:
 
         return True
 
-    def load_stored_model(self, folder: str) -> bool:
+    def load_stored_model(self, folder: str,
+                          precision: Literal["fp32", "fp16", "int8"] = "fp32"
+                          ) -> bool:
+        # Check precision
+        if precision not in self._precision_vals:
+            print(self._RED + "Precision value not valid" + self._RST)
+            return False
+
         # Check if already loaded
         if self._model_init:
             print(self._RED + "Model already loaded!" + self._RST)
@@ -84,7 +114,19 @@ class GPT2Wrapper:
                 raise FileNotFoundError(f"Folder '{folder}' not found")
 
             # Load model
-            self._model = GPT2LMHeadModel.from_pretrained(folder)
+            kwargs = {}
+
+            if precision == "int8":
+                bnb_config = BitsAndBytesConfig(load_in_8bit=True)
+                kwargs["quantization_config"] = bnb_config
+            else:
+                dtype = torch.float16 if precision == "fp16" else torch.float32
+                kwargs["dtype"] = dtype
+
+            self._model = GPT2LMHeadModel.from_pretrained(
+                folder,
+                **kwargs
+            )
             # Load tokenizer
             self._tokenizer = GPT2Tokenizer.from_pretrained(folder)
         except FileNotFoundError as e:
@@ -105,7 +147,15 @@ class GPT2Wrapper:
 
         return True
 
-    def load_stored_peft_model(self, folder: str) -> bool:
+    def load_stored_peft_model(
+        self, folder: str,
+        precision: Literal["fp32", "fp16", "int8"] = "fp32"
+    ) -> bool:
+        # Check precision
+        if precision not in self._precision_vals:
+            print(self._RED + "Precision value not valid" + self._RST)
+            return False
+
         # Check if already loaded
         if self._model_init:
             print(self._RED + "Model already loaded!" + self._RST)
@@ -120,8 +170,18 @@ class GPT2Wrapper:
                 raise FileNotFoundError(f"Folder '{folder}' not found")
 
             # Load model
+            kwargs = {}
+
+            if precision == "int8":
+                bnb_config = BitsAndBytesConfig(load_in_8bit=True)
+                kwargs["quantization_config"] = bnb_config
+            else:
+                dtype = torch.float16 if precision == "fp16" else torch.float32
+                kwargs["dtype"] = dtype
+
             self._base_model = GPT2LMHeadModel.from_pretrained(
-                self._model_name
+                self._model_name,
+                **kwargs
             )
             # Load tokenizer
             self._tokenizer = GPT2Tokenizer.from_pretrained(self._model_name)
@@ -318,7 +378,6 @@ class GPT2Wrapper:
             args=training_args,
             train_dataset=tokenized_datasets["train"],  # subset for testing
             eval_dataset=tokenized_datasets["validation"],
-            tokenizer=self._tokenizer,
             data_collator=data_collator
         )
 

qwen_wrapper/qwen_wrapper.py
@@ -5,9 +5,9 @@ from typing import Literal
 import torch
 from datasets import DatasetDict
 from peft import LoraConfig, PeftModel, TaskType, get_peft_model
-from transformers import (AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig,
-                          DataCollatorForLanguageModeling, Trainer,
-                          TrainingArguments)
+from transformers import (AutoModelForCausalLM, AutoTokenizer,
+                          BitsAndBytesConfig, DataCollatorForLanguageModeling,
+                          Trainer, TrainingArguments)
 
 
 class QwenType(Enum):
@@ -50,8 +50,8 @@ class QwenWrapper:
             self._device = torch.device("cpu")
 
     def load_pretrained_model(
-            self,
-            precision: Literal["fp32", "fp16", "int8"] = "fp32"
+        self,
+        precision: Literal["fp32", "fp16", "int8"] = "fp32"
     ) -> bool:
         # Check precision
         if precision not in self._precision_vals: